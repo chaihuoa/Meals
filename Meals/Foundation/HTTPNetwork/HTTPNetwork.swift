ca038e2caa3bb035b66e8709be73c0e398223064
Yongfeng <bravetsia@gmail.com>
2023-01-13 20:25:38 -0600

    Created a HTTPNetwork class.

---
+++ Meals/Foundation/HTTPNetwork/HTTPNetwork.swift
@@ -0,0 +1,66 @@
+//
+//  HTTPNetwork.swift
+//  Meals
+//
+//  Created by Yongfeng on 1/13/23.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case GET
+    case POST
+}
+
+protocol API {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var params: [URLQueryItem]? { get }
+}
+
+final class NetworkManager {
+    
+    private class func buildURL(endpoint: API) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = endpoint.baseURL
+        components.path = endpoint.path
+        components.queryItems = endpoint.params
+        
+        return components
+    }
+    
+    class func request<T: Decodable>(endpoint: API, completion: @escaping (Result<T, Error>) -> Void) {
+        let components = buildURL(endpoint: endpoint)
+        guard let url = components.url else {
+            print("URL creation error")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard response != nil, let data = data else {
+                return
+            }
+            
+            if let responseObject = try? JSONDecoder().decode(T.self, from: data) {
+                completion(.success(responseObject))
+            } else {
+                let error = NSError(domain: "com.meals", code: 200, userInfo: [NSLocalizedDescriptionKey: "Failed"])
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
